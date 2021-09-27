f0b077d7d9aae4586bf6b50b290b496d42b6e0f5
yuzhihui <844370396@qq.com>
2021-09-27 11:59:20 +0800

    文件上传余下载

--- test4.py
+++ test4.py
@@ -2,18 +2,42 @@ a = 0
 
 
 class Animal:
+    # 单例模式
+    def __new__(cls, *args, **kwargs):
+        if cls is None:
+            cls = super(Animal).__new__()
+        return cls
+
     def eat(self):
+        print("吃")
+        print("吃")
         print("吃")
 
 
 class Cat(Animal):
-    def __init__(self, name, weight):
-        self.name = name
-        self.weight = weight
+    # def __init__(self, name, weight):
+    #     self.name = name
+    #     self.weight = weight
 
-    def __str__(self):
-        return "zhe shi yi ge kong fang fa %s :%d" % (self.name, self.weight)
+    # def __str__(self):
+    #     return "zhe shi yi ge kong fang fa %s :%d" % (self.name, self.weight)
+    pass
 
 
-cat = Cat("xiao", 77)
-print(cat.eat())
+# cat = Cat()
+# print(cat.eat())
+
+
+def main():
+    print("这是一个主方法")
+
+
+if __name__ == "__main__":
+    print("执行了主方法")
+    main()
+
+file = open("readme.txt")
+text = file.read()
+file.write("yuzh844e")
+print(text)
+file.close()
---
+++ test5.py
@@ -0,0 +1,11 @@
+import random
+
+a = random.__file__
+print(a)
+
+rand = random.randint(0, 100)
+print(rand)
+
+
+def method():
+    pass
